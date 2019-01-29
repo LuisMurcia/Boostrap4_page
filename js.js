871d93f15fd349a6d030428b45e0386035d5adbd
Luis <luismurcia97@gmail.com>
2019-01-29 19:59:54 +0100

    LuisMurcia

--- js.js
+++ js.js
@@ -1,8 +1,9 @@
-﻿$(function(){
-  $('scroll').click(function () {
-    $('html, body').animate({
-       scrollTop: $(document).height()
-    }, 'slow');
-    return false;
-  });
+﻿$(document).ready(function(){
+	var a = $('.nav-link');
+	a.click(function(){
+	    var section = $(this).attr('href');
+	    $('html, body').animate({
+	      scrollTop: $(section).offset().top
+	    }, 1000);
+	});
 });
